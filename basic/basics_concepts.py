a91c74adfc55fea748f437baf6cd5030c9ec8a75
victoralc <victoralcantara432@gmail.com>
2018-06-07 20:28:00 -0300

    basic python

---
+++ basic/basics_concepts.py
@@ -0,0 +1,3 @@
+print("Welcome","to", "Python","programming", sep="-")
+nome = "Carlos Victor de Alcantara Carleial"
+age = 29,0
---
+++ challenges/print_function.py
@@ -0,0 +1,4 @@
+number = input("Insert a number: ")
+
+for n in range(1, int(number)+1):
+    print(n, end="")
